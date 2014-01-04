65025a8043ff02f76b8f1b34cc7f49da88bd5b5f
Kouhei Sutou <kou@clear-code.com>
2014-01-04 20:31:55 +0900

    Add TextData for text data

--- lib/chupa-text/text-data.rb
+++ lib/chupa-text/text-data.rb
@@ -14,24 +14,13 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "chupa-text/version"
-
-require "chupa-text/configuration"
-require "chupa-text/configuration-loader"
-require "chupa-text/decomposer"
-require "chupa-text/decomposer-registry"
-require "chupa-text/decomposers"
-require "chupa-text/extractor"
-require "chupa-text/formatters"
-require "chupa-text/mime-type"
-require "chupa-text/mime-type-registry"
-
-require "chupa-text/file-content"
-require "chupa-text/virtual-content"
-
-require "chupa-text/data"
-require "chupa-text/input-data"
-require "chupa-text/virtual-file-data"
-require "chupa-text/text-data"
-
-require "chupa-text/command"
+module ChupaText
+  class TextData < Data
+    def initialize(text)
+      super()
+      self.mime_type = "text/plain"
+      self.body = text
+      self.size = text.bytesize
+    end
+  end
+end
--- test/test-text-data.rb
+++ test/test-text-data.rb
@@ -14,24 +14,23 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "chupa-text/version"
+class TestTextData < Test::Unit::TestCase
+  def test_mime_type
+    assert_equal("text/plain", text_data("").mime_type)
+  end
 
-require "chupa-text/configuration"
-require "chupa-text/configuration-loader"
-require "chupa-text/decomposer"
-require "chupa-text/decomposer-registry"
-require "chupa-text/decomposers"
-require "chupa-text/extractor"
-require "chupa-text/formatters"
-require "chupa-text/mime-type"
-require "chupa-text/mime-type-registry"
+  def test_body
+    body = "Hello"
+    assert_equal(body, text_data(body).body)
+  end
 
-require "chupa-text/file-content"
-require "chupa-text/virtual-content"
+  def test_size
+    body = "Hello"
+    assert_equal(body.bytesize, text_data(body).size)
+  end
 
-require "chupa-text/data"
-require "chupa-text/input-data"
-require "chupa-text/virtual-file-data"
-require "chupa-text/text-data"
-
-require "chupa-text/command"
+  private
+  def text_data(text)
+    ChupaText::TextData.new(text)
+  end
+end
