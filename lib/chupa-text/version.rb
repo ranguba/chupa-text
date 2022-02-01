f312718e51f380577dcf47e02f10d80dc3484636
Sutou Kouhei <kou@clear-code.com>
2022-02-01 10:50:23 +0900

    Bump version

--- lib/chupa-text/version.rb
+++ lib/chupa-text/version.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2013-2020  Sutou Kouhei <kou@clear-code.com>
+# Copyright (C) 2013-2022  Sutou Kouhei <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -15,5 +15,5 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 module ChupaText
-  VERSION = "1.3.3"
+  VERSION = "1.3.4"
 end
