b46877e75f065f8ac2d42df89028d1ad7ec817bd
Sutou Kouhei <kou@clear-code.com>
2024-09-22 17:38:32 +0900

    Suppress "literal string will be frozen" warnings

--- lib/chupa-text/decomposers/xml.rb
+++ lib/chupa-text/decomposers/xml.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2013-2019  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2013-2024  Sutou Kouhei <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -29,7 +29,7 @@ module ChupaText
       end
 
       def decompose(data)
-        text = ""
+        text = +""
         listener = Listener.new(text)
         data.open do |input|
           begin
